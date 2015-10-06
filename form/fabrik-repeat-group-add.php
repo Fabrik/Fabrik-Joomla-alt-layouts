9092f903cac4359a7735abc2a69a6e07f93b73f6
pollen8 <rob@pollen-8.co.uk>
2015-10-06 18:44:16 +0200

    form repeat group buttons

---
+++ form/fabrik-repeat-group-add.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Repeat group add button
+ */
+
+defined('JPATH_BASE') or die;
+
+$d = $displayData;
+?>
+<a class="addGroup btn btn-small btn-success" href="#" data-toggle="tooltip" title="<?php echo FText::_('COM_FABRIK_ADD_GROUP');?>">
+	<?php echo  FabrikHelperHTML::icon('icon-plus');?>
+</a>
+
