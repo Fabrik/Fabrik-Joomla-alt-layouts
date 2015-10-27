289f2c795990502b4d1ad2ac779d69c8a1326800
pollen8 <rob@pollen-8.co.uk>
2015-10-27 10:53:59 +0100

    footer changes, element labels updated

--- fabrik-icon.php
+++ fabrik-icon.php
@@ -6,31 +6,41 @@
 
 defined('JPATH_BASE') or die;
 
-$d = $displayData;
+$d     = $displayData;
 $props = isset($d->properties) ? $d->properties : '';
 
 $test = str_replace('icon-', '', trim($d->icon));
+$test = explode(' ', $test);
 
-switch ($test) {
-	case 'list-view':
-		$d->icon = 'fa-list';
-		break;
-	case 'feed':
-		$d->icon = 'fa-rss';
-		break;
-	case 'picture':
-		$d->icon = 'fa-picture-o';
-		break;
-	case 'delete':
-		$d->icon = 'fa-times';
-		break;
-	case 'expand-2':
-		$d->icon = 'fa-expand';
-		break;
-	default :
-		$d->icon = str_replace('icon-', 'fa-', $d->icon);
-		break;
+foreach ($test as &$t)
+{
+	switch ($t)
+	{
+		case 'list-view':
+			$t = 'fa-list';
+			break;
+		case 'feed':
+			$t = 'fa-rss';
+			break;
+		case 'picture':
+			$t = 'fa-picture-o';
+			break;
+		case 'delete':
+			$t = 'fa-times';
+			break;
+		case 'expand-2':
+			$t = 'fa-expand';
+			break;
+		case 'eye-open':
+			$t = 'fa-eye';
+			break;
+		default :
+			$t = str_replace('icon-', 'fa-', $d->icon);
+			break;
+	}
 }
+
+$d->icon = implode(' ', $test);
 ?>
 
-<span class="fa <?php echo $d->icon;?>" <?php echo $props;?>></span>
+<span class="fa <?php echo $d->icon; ?>" <?php echo $props; ?>></span>
