5b1bd0a9eb252b780ce5e34707ba56c5036f1311
Hugh Messenger <hugh.messenger@gmail.com>
2016-02-04 00:51:15 -0600

    Added data-role to group +/- icons

--- bootstrap3/form/fabrik-repeat-group-add.php
+++ bootstrap3/form/fabrik-repeat-group-add.php
@@ -8,6 +8,6 @@ defined('JPATH_BASE') or die;
 $d = $displayData;
 ?>
 <a class="addGroup btn btn-small btn-success" href="#" data-toggle="tooltip" title="<?php echo FText::_('COM_FABRIK_ADD_GROUP');?>">
-	<?php echo  FabrikHelperHTML::icon('icon-plus');?>
+	<?php echo  FabrikHelperHTML::icon('icon-plus', '', 'data-role="fabrik_duplicate_group"');?>
 </a>
 
--- bootstrap3/form/fabrik-repeat-group-delete.php
+++ bootstrap3/form/fabrik-repeat-group-delete.php
@@ -8,5 +8,5 @@ defined('JPATH_BASE') or die;
 $d = $displayData;
 ?>
 <a class="deleteGroup btn btn-small btn-danger" href="#"  data-toggle="tooltip" title="<?php echo FText::_('COM_FABRIK_DELETE_GROUP');?>">
-	<?php echo FabrikHelperHTML::icon('icon-minus'); ?>
+	<?php echo FabrikHelperHTML::icon('icon-minus', '', 'data-role="fabrik_delete_group"'); ?>
 </a>
