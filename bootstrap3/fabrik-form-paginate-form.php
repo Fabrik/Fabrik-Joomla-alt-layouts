1b2b60120e97ff5cdca99ea477bfc75217f48710
Hugh Messenger <hugh.messenger@gmail.com>
2018-03-28 16:08:21 -0500

    Override for paginate form plugin

---
+++ bootstrap3/fabrik-form-paginate-form.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Fabrik.form.paginate
+ * @copyright   Copyright (C) 2005-2016  Media A-Team, Inc. - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+defined('_JEXEC') or die;
+$start = FText::_('COM_FABRIK_START');
+$next = FText::_('COM_FABRIK_NEXT');
+$prev = FText::_('COM_FABRIK_PREV');
+$end = FText::_('COM_FABRIK_END');
+?>
+<nav>
+    <ul class="pagination">
+        <li data-paginate="first" class="pagination-start <?php echo $displayData['first-active'] ? 'active' : ''?>">
+            <a class="pagenav" href="<?php echo $displayData['first'];?>" title="<?php echo $start; ?>">
+                <?php echo $start; ?>
+            </a>
+        </li>
+        <li data-paginate="prev" class="pagination-prev <?php echo $displayData['first-active'] ? 'active' : ''?>">
+            <a class="pagenav" href="<?php echo $displayData['prev'];?>" title="<?php echo $prev; ?>">
+                <?php echo $prev; ?>
+            </a>
+        </li>
+        <li data-paginate="next" class="pagination-next <?php echo $displayData['last-active'] ? 'active' : ''?>">
+            <a class="pagenav" href="<?php echo $displayData['next'];?>" title="<?php echo $next; ?>">
+                <?php echo $next; ?>
+            </a>
+        </li>
+        <li data-paginate="last" class="pagination-end <?php echo $displayData['last-active'] ? 'active' : ''?>">
+            <a class="pagenav" href="<?php echo $displayData['last'];?>" title="<?php echo $end; ?>">
+                <?php echo $end; ?>
+            </a>
+        </li>
+    </ul>
+</nav>
